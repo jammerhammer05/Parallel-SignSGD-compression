4c131fe300c5d33ecc9861905e0ad73635730400
2019201069 <54199372+2019201069@users.noreply.github.com>
2020-05-04 13:33:07 +0530

    compress and decompress of the signs

---
+++ compdecomptest.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+#
+
+import numpy as np
+
+def compress(theta_grad):
+	
+	shape_theta = theta_grad.shape
+
+	print("shape :", shape_theta)
+	loops = shape_theta[0]
+	itr = shape_theta[1]
+	reslist = []
+	
+	while(loops > 0):
+
+		res = 0
+		itr = shape_theta[1]
+		i = 0
+		while(itr > 0):
+	        
+			x = int(theta_grad[loops-1][itr-1])
+			if(x == -1):
+				res =  res + 2 * pow(3,i)
+			elif(x == 0):
+				res =  res 
+			elif(x == 1):
+				res =  res + 1 * pow(3, i)
+			else :
+				print("Error in signSGD compression")
+				exit(0)
+
+			itr = itr - 1
+			i = i + 1
+
+		reslist.append(res)
+		loops = loops - 1
+
+	return shape_theta[1], reslist
+
+def ternary (n):
+    if n == 0:
+        return '0'
+    nums = []
+    while n:
+        n, r = divmod(n, 3)
+        nums.append(str(r))
+    return ''.join(reversed(nums))
+
+def decompress(t_c, l):
+    
+	t_len = len(t_c)
+	reslist = []
+	i = 0
+
+	while (i < t_len):
+
+		res = []
+		t = t_c[i]
+
+		while t:
+			t, r = divmod(t, 3)
+			if(r == 2):
+				res.append(-1)
+			else:
+				res.append(r)
+
+		lenlist = len(res)
+
+		if(lenlist != l):
+			dif = l - lenlist
+			while dif:
+				res.append(0)
+				dif = dif-1
+
+		print(res)
+		res.reverse()
+		i = i+1
+		reslist.append(res)
+
+	reslist.reverse()
+	resnp = np.array(reslist)
+	return resnp
+
+def compare(t1, t2):
+	if(t1 == t2):
+		return True
+	return False
+
+t1 = [[-1., -1., -1., -1., -1., 0., -1., -1., -1., -1., -1., -1., -1., -1., -1., -1., -1., -1.,
+  -1., -1., -1., -1., -1., -1., -1., -1.],
+ [ 1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  0.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  0.,  1.,
+   1.,  1.,  1.,  1.,  1.,  1.,  1.,  1.]]
+
+# t1 = [[0,1,-1],[1,0,-1],[0,0,1]]
+
+t1_np = np.array(t1)
+print(t1_np)
+
+l, t1_comp = compress(t1_np)
+print("t1_comp:\n", t1_comp)
+
+t1_d = decompress(t1_comp, l)
+print("the result is :\n",t1_d)
+# if(compare(t1_d, t1)):
+# 	print("YES")
+# else :
+# 	print("NO")
